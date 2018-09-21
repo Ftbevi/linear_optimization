8567c9359c8ecf6a470b3d7d8031ea71dcb1bd41
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2018-09-21 10:28:52 -0300

    rafactoring item_a.

--- item_a.m
+++ item_a.m
@@ -14,23 +14,24 @@ for i = 1: disciplinas_m
 endfor
 
 % criar vetor b
-b = zeros(1, disciplinas_m);
-for i = 1: disciplinas_m
-  b(1, i) = 1;
-endfor
-c(1:50) = [8 8 7 9 2 8 1 2 7 3 10 7 7 9 5 10 8 8 10 3 10 5 5 7 5 6 10 2 7 9 5 8 10 5 2 8 8 9 3 4 5 8 10 5 1 7 1 1 1 6]';
+b = ones(1, disciplinas_m);
 
 % carregar vetor c
-%aux = load('alocacao.txt')
-%c = zeros(1, disciplinas_m * professores_n);
-%for i = 1: disciplinas_m
-%  for j = 1: professores_n
-%    c.(aux(i, j);)
-%  endfor
-%endfor
+aux = load('alocacao.txt');
+c = zeros(1, disciplinas_m * professores_n);
+marcador_aux = 0;
+for i = 1: disciplinas_m
+  for j = 1: professores_n
+    marcador_aux+=1;
+    c(1,marcador_aux) = (aux(i, j));
+  endfor
+endfor
 
-A;
-b;
-c;
+% Metodo simplex
+%   No metodo e colocado -c para que o problema se torne
+% de maximizacao.
+[x_otimo, custo_reduzido] = glpk(-c,A,b);
 
-[x_otimo, custo_otimo] =  [x_otimo, custo_reduzido] = glpk(-c,A,b)
+% Mostra x no ponto otimo e custo reduzido.
+x_otimo'
+custo_reduzido
