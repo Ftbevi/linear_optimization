5c2b9b9c61ee337695293c3e1537e99791c1d23a
Thiago Bevilaqua <fthiagobevilaqua@gmail.com>
2018-09-20 23:54:01 -0300

    new questions

--- item_a.m
+++ item_a.m
@@ -12,19 +12,25 @@ for i = 1: disciplinas_m
   endfor
    marcador += 5;
 endfor
-A
 
 % criar vetor b
 b = zeros(1, disciplinas_m);
 for i = 1: disciplinas_m
   b(1, i) = 1;
 endfor
-b
+c(1:50) = [8 8 7 9 2 8 1 2 7 3 10 7 7 9 5 10 8 8 10 3 10 5 5 7 5 6 10 2 7 9 5 8 10 5 2 8 8 9 3 4 5 8 10 5 1 7 1 1 1 6]';
 
 % carregar vetor c
-aux = load('alocacao.txt')
-c = zeros(1, disciplinas_m * professores_n);
-for i = 1: disciplinas_m * professores_n
-  c(1, i)= aux(i);
-endfor
-c
+%aux = load('alocacao.txt')
+%c = zeros(1, disciplinas_m * professores_n);
+%for i = 1: disciplinas_m
+%  for j = 1: professores_n
+%    c.(aux(i, j);)
+%  endfor
+%endfor
+
+A;
+b;
+c;
+
+[x_otimo, custo_otimo] =  [x_otimo, custo_reduzido] = glpk(-c,A,b)
